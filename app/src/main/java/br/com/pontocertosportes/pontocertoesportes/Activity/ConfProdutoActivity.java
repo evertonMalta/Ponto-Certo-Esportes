f18490cbc1ada157df778cad516703aa32c977f7
Brendhow <brendhowpereira@hotmail.com>
2018-11-30 17:52:11 -0200

    product & Vendas

---
+++ app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/ConfProdutoActivity.java
@@ -0,0 +1,13 @@
+package br.com.pontocertosportes.pontocertoesportes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf_produto);
+    }
+}
---
+++ app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/ConfVendasActivity.java
@@ -0,0 +1,13 @@
+package br.com.pontocertosportes.pontocertoesportes.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfVendasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf_vendas);
+    }
+}
