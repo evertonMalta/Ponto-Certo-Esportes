e625081ea724ad39478551dc47be00103a3cf969
evertonMalta <everton.malta.7@gmail.com>
2018-12-01 18:37:50 -0200

    new fix view client

--- app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/ViewClientActivity.java
+++ app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/ViewClientActivity.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 import br.com.pontocertosportes.pontocertoesportes.R;
 
 
-<<<<<<< HEAD:app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/ViewClientActivity.java
+
 public class ViewClientActivity extends AppCompatActivity {
-=======
-public class EditClientActivity extends AppCompatActivity {
->>>>>>> de07006f8bbc00655d50ee36cf0900a950ad80ff:app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/EditClientActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
